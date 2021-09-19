2fa02fcffddefa97ad9203f87086fab7736f8320
Алексей Попков <alexey.b.popkov@gmail.com>
2021-09-19 13:26:28 +0300

    Initial

--- Задание №3.py
+++ Задание №3.py
@@ -1,7 +1,9 @@
 numbers = [0, 99, 100, 53, 44, 23, 4, 8, 16, 15, 77, 51]
 
-def oddQ(n):
+
+def f(n):
     if n % 2 != 0 and n > 50: return True
     return False
 
-print(list(filter(oddQ, numbers)))
+
+print(list(filter(f, numbers)))
